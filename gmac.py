c1105e2550874d98f5a0d3c3701948428188ef69
Ngoc Tran <hello@trannn.ml>
2020-08-26 16:15:59 +0700

    add * and / on Polynomials

--- gmac.py
+++ gmac.py
@@ -1,6 +1,11 @@
 from utils import AES_encrypt, generate_key
 from struct import pack
 
+
+def _deg(x):
+    assert x >= 0
+    return x.bit_length() if x > 0 else -1
+
 class GF2p128:
     def __init__(self, val, m=0x100000000000000000000000000000087):
         self.val = val
@@ -11,12 +16,6 @@ class GF2p128:
     
     def __repr__(self):
         return repr(self.val)
-
-    def deg(self):
-        x = self.val
-        assert 0 <= x < 2 ** 128, "Number not in GF(2^128)!"
-        if x == 0: return -1
-        return x.bit_length()
 
     def __add__(self, obj):
         assert self.m == obj.m, "Cannot add/sub numbers of different polynomial generators!"
@@ -36,7 +35,7 @@ class GF2p128:
             a >>= 1
             b <<= 1
 
-            if b.bit_length() == m.bit_length():
+            if _deg(b) == _deg(m):
                 b ^= m
 
         return GF2p128(p, m)
@@ -69,8 +68,8 @@ class GF2p128:
         assert self.m == obj.m, "Cannot divmod numbers of different polynomial generators!"
         q, r, b, m = 0, self.val, obj.val, self.m
 
-        while r.bit_length() >= b.bit_length():
-            d = r.bit_length() - b.bit_length()
+        while _deg(r) >= _deg(b):
+            d = _deg(r) - _deg(b)
             q = q ^ (1 << d)
             r = r ^ (b << d)
 
@@ -117,3 +116,100 @@ def gmac(key, msg, nonce):
     g += s
     mac = int.to_bytes(g.val, 16, 'big')
     return encrypted, mac
+
+
+class Polynomial:
+    def __init__(self, coeff, num_class=GF2p128):
+        self.coeff = coeff[:]
+        self.num_class = GF2p128
+
+    def __eq__(self, obj):
+        return len(self.coeff) == len(obj.coeff) and all([x == y for (x, y) in zip(self.coeff, obj.coeff)])
+
+    def copy(self):
+        return Polynomial(self.coeff[:], self.num_class)
+
+    def deg(self):
+        return len(self.coeff) - 1
+    
+    def __call__(self, val):
+        ret = 0
+        for coeff in reversed(self.coeff):
+            ret *= val
+            ret += coeff
+        return ret
+
+    def __add__(self, obj):
+        assert type(self) == type(obj), "Can only add Polynomials with Polynomials!"
+        return Polynomial([x + y for (x, y) in zip(self.coeff, obj.coeff)], self.num_class)
+
+    def __sub__(self, obj):
+        assert type(self) == type(obj), "Can only subtract Polynomials with Polynomials!"
+        return Polynomial([x - y for (x, y) in zip(self.coeff, obj.coeff)], self.num_class)
+
+    def __mul__(self, obj):
+        if type(obj) is self.num_class:
+            return Polynomial([x * obj for x in self.coeff])
+        assert type(self) is type(obj), "Can only multiply Polynomials with Polynomials!"
+        total_deg = self.deg() + obj.deg()
+        coeffs = [self.num_class()] * (total_deg + 1)
+        for idx1, val1 in enumerate(self.coeff):
+            for idx2, val2 in enumerate(obj.coeff):
+                coeffs[total_deg - idx1 - idx2 + 1] += val1 * val2
+        return Polynomial(coeffs, self.num_class)
+
+    def __truediv__(self, obj):
+        if type(obj) is self.num_class:
+            return Polynomial([x / obj for x in self.coeff])
+        return divmod(self, obj)[0]
+
+    def __mod__(self, obj):
+        return divmod(self, obj)[1]
+
+    def __divmod__(self, obj):
+        assert type(self) is type(obj), "Can only divide Polynomials with Polynomials!"
+        
+        divider = self.copy()
+        coeffs = []
+        while divider.deg() >= obj.deg():
+            coeff = self[0] / obj[0]
+            divided = obj.copy()
+            divided.coeff += [GF2p128()] * (divider.deg() - obj.deg())
+            divider -= divided * coeff
+            coeffs.append(coeff)
+
+        return Polynomial(coeffs, self.num_class), divider
+
+    def monic(self):
+        lead_coeff = self.coeff[0]
+        return Polynomial([x / lead_coeff for x in self.coeff], self.num_class)
+
+    def sqr_free_factor(self):
+        ...
+
+    def diff_deg_factor(self):
+        ...
+
+    def eq_deg_factor(self, degree):
+        # Cantor-Zassenhaus algorithm for equal-degree factorization.
+        f, d = self, degree
+        n = f.deg()
+        r = n // d
+        S = {f}
+
+        while len(S) < r:
+            h = random_polynomial(1, f)
+            g = gcd(h, f)
+
+            if g == 1:
+                g = (h ** ((q ** d - 1) / 3) - 1) % f
+
+            for u in S:
+                if u.deg() = d:
+                    continue
+                
+                gcd_gu = gcd(g, u)
+                if gcd_gu != 1 and gcd_gu != u:
+                    S = (S - {u}) | ({gcd_gu, u / gcd_gu})
+
+        return S
