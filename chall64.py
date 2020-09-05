91801efb8627560a6ed2cb288f7df292c9be7551
tran.ngo.quang.ngoc <SUN-ASTERISK\tran.ngo.quang.ngoc@b120639-pc3.sun-asterisk.com>
2020-09-05 09:48:37 +0700

    chall64 code

--- chall64.py
+++ chall64.py
@@ -1,118 +1,23 @@
-import numpy as np
-from gmac import gmac, GF2p128, Polynomial
-from tqdm import trange
-from utils import generate_key, AES_encrypt
-from struct import pack
-from random import seed
+from multiprocessing import Pool, cpu_count
+from chall64_helper import *
 
-# for reproducibility
-seed(123)
-np.random.seed(456)
+# accumulator through the iterations
+X = np.eye(128, dtype=np.int8)
 
-def block2gf(block):
-    assert len(block) == 16
-    return GF2p128(int.from_bytes(block, 'big'))
+key = b'harem_enthusiast'
+authkey = gf2vec(block2gf(AES_encrypt(key, b'\x00' * 16)))
+msg = generate_key(2 ** n * 16 - 8)
+nonce = generate_key(12)
+encrypted, signature = gmac(key, msg, b'', nonce)
+# get the last 32 bit
+trunc_size = 8 * 4
+signature = signature[-trunc_size // 8:]
+assert len(encrypted) == 2 ** n * 16
 
-def gf2vec(val: GF2p128):
-    ret = np.empty((128,), dtype=np.int8)
-    val = val.val
-    for i in range(128):
-        ret[i] = val & 1
-        val >>= 1
-    return ret
-
-sqr_mat = np.empty((128, 128), dtype=np.int8)
-for i in range(128):
-    sqr_mat[:, i] = gf2vec(GF2p128(1 << i) ** 2)
-
-def gf2mat(val: GF2p128):
-    ret = np.empty((128, 128), dtype=np.int8)
-    acc = GF2p128(1)
-    for i in range(128):
-        ret[:, i] = gf2vec(val * acc)
-        acc.val <<= 1
-    return ret
-
-def vec2gf(vec: np.array):
-    ret = 0
-    for bit in reversed(vec):
-        ret <<= 1
-        ret |= int(bit)
-    return GF2p128(ret)
-
-def gaussian_nullspace(mat):
-    mat = mat.T
-    target = np.eye(mat.shape[0], dtype=np.int8)
-    idx = 0
-    rank = 0
-    while True:
-        if idx == mat.shape[1] or rank == min(mat.shape):
-            break
-        row_idx = np.flatnonzero(mat[rank:, idx]) + rank
-        idx += 1
-        if len(row_idx) == 0: continue
-        if row_idx[0] != rank:
-            # swap
-            mat[[rank, row_idx[0]]] = mat[[row_idx[0], rank]]
-            target[[rank, row_idx[0]]] = target[[row_idx[0], rank]]
-        # now subtract from the rest
-        for idx_ in row_idx[1:]:
-            mat[idx_, :] = (mat[idx_, :] - mat[rank, :]) % 2
-            target[idx_, :] = (target[idx_, :] - target[rank, :]) % 2
-        rank += 1
-
-    # transpose so column combination is easier
-    target = target[rank:, :].T
-    # remove duplicate vectors
-    target = np.unique(target, axis=1)
-    # remove zero vector if exists
-    target = target[:, np.any(target, axis=0)]
-    return target
-
-
-test = block2gf(b"One day I'm gon'")
-assert (gf2vec(test ** 2) == (sqr_mat @ gf2vec(test)) % 2).all()
-test2 = block2gf(b'marry a pornstar')
-assert (gf2vec(test * test2) == (gf2mat(test2) @ gf2vec(test)) % 2).all()
-assert (gf2vec(test * test2) == (gf2mat(test) @ gf2vec(test2)) % 2).all()
-assert (gf2vec(test ** 16) == (np.linalg.matrix_power(sqr_mat, 4) @ gf2vec(test)) % 2).all()
-assert (gf2vec(test2 ** 8) == (sqr_mat @ sqr_mat @ sqr_mat @ gf2vec(test2)) % 2).all()
-
-def get_Ad(blocks):
-    # higher order/beginning of blocks first, based on Horner's method
-    # remember that this only deals with 2^i-th blocks.
-    acc = np.zeros((128, 128), dtype=np.int8)
-    for block in blocks:
-        acc = ((gf2mat(block2gf(block)) + acc) @ sqr_mat) % 2
-    return (acc @ sqr_mat) % 2
-
-def get_Ad_loc(i):
-    block_idx = i // 128
-    cell_idx = i % 128
-    byte_idx = cell_idx // 8
-    bit_idx = cell_idx % 8
-    payload = [bytes(16)] * (1 + block_idx) + \
-                [bytes(byte_idx) + bytes([1 << bit_idx]) + bytes(15 - byte_idx)] + \
-                [bytes(16)] * (127 - block_idx)
-    return get_Ad(payload)
-
-def gmac_ok(key, cipher, mac, nonce):
-    '''
-    Input:
-        @key:       key to be encrypted/GMAC
-        @cipher:    cipher to be decrypted
-        @mac:       the generated MAC to be checked
-        @nonce:     96-bit of nonce to XOR at the end
-    '''
+def gmac_ok(cipher, mac):
     authkey = AES_encrypt(key, b'\x00' * 16)
     authkey = GF2p128(int.from_bytes(authkey, 'big'))
-    if len(cipher) == 0:
-        iv = encrypted = b''
-    else:
-        iv = cipher[:8]
-        encrypted = cipher[8:]
-    content = iv + encrypted + b'\x00' * (-len(iv + encrypted) % 16) + \
-              pack('>2Q', 0, len(iv + encrypted))
+    content = cipher + b'\x00' * (-len(cipher) % 16) + pack('>2Q', 0, len(cipher))
     g = GF2p128(0)
     for i in range(0, len(content), 16):
         b = GF2p128(int.from_bytes(content[i : i + 16], 'big'))
@@ -121,20 +26,32 @@ def gmac_ok(key, cipher, mac, nonce):
     s = AES_encrypt(key, nonce + b'\x00\x00\x00\x01')
     s = GF2p128(int.from_bytes(s, 'big'))
     g += s
-    return int.to_bytes(g.val, 16, 'big').startswith(mac)
+        
+    return int.to_bytes(g.val, 16, 'big')[-trunc_size // 8:] == mac
 
-n = 16
-# accumulator through the iterations
-X = np.eye(128)
-Ad_0 = get_Ad([b'\x00' * 16] * n)
-
-from joblib import Parallel, delayed
-def get_dependency_matrix():
-    # rows = bits in Ad*X, col = bits in blocks
-    Ad0_ = Ad_0 @ X
+pool = Pool(cpu_count())
+while X.shape[1] > 1:
+    print(X.shape[1], 'basis vectors left.')
+    # no_of_zero_rows = min(n * 128 // X.shape[1] - 1, trunc_size - 1)
     no_of_zero_rows = n * 128 // X.shape[1] - 1
-    def get_col(bit_idx):
-        return ((get_Ad_loc(bit_idx) @ X)[:no_of_zero_rows, :] != Ad0_[:no_of_zero_rows, :]).astype(np.int8).flatten()
-    return np.stack(Parallel(n_jobs=8)(delayed(get_col)(row_idx) for row_idx in trange(n * 128)), axis=1)
+    print('Zeroing out', no_of_zero_rows, 'rows.')
+    dependency = get_dependency_matrix(no_of_zero_rows, X)
+    nullspace = gaussian_nullspace(dependency)
 
-print(get_dependency_matrix())
+    print('Rolling the dice until dawn...')
+    set_value(0)
+    nullvec = pool.starmap(
+        try_nullvec,
+        [(gmac_ok, nullspace, encrypted, signature)] * cpu_count()
+    )
+    for i in nullvec:
+        if i is not None:
+            nullvec = i
+            break
+
+    new_nullspace = (get_Ad(nullvec)[no_of_zero_rows:trunc_size] @ X) % 2
+    new_domain = gaussian_nullspace(new_nullspace)
+    X = (X @ new_domain) % 2
+
+assert authkey == X.T
+
---
+++ chall64_helper.py
@@ -0,0 +1,129 @@
+from multiprocessing import Value
+import numpy as np
+from gmac import gmac, GF2p128, Polynomial
+from tqdm.auto import trange, tqdm
+from utils import generate_key, AES_encrypt
+from struct import pack
+from joblib import Parallel, delayed
+
+# 2^n number of blocks
+n = 16
+
+def block2gf(block):
+    assert len(block) == 16
+    return GF2p128(int.from_bytes(block, 'big'))
+
+def gf2vec(val: GF2p128):
+    ret = np.empty((128,), dtype=np.int8)
+    val = val.val
+    for i in range(128):
+        ret[i] = val & 1
+        val >>= 1
+    return ret
+
+sqr_mat = np.empty((128, 128), dtype=np.int8)
+for i in range(128):
+    sqr_mat[:, i] = gf2vec(GF2p128(1 << i) ** 2)
+
+def gf2mat(val: GF2p128):
+    ret = np.empty((128, 128), dtype=np.int8)
+    acc = GF2p128(1)
+    for i in range(128):
+        ret[:, i] = gf2vec(val * acc)
+        acc.val <<= 1
+    return ret
+
+def vec2gf(vec: np.array):
+    ret = 0
+    for bit in reversed(vec):
+        ret <<= 1
+        ret |= int(bit)
+    return GF2p128(ret)
+
+def vec2block(vec: np.array):
+    ret = 0
+    for bit in reversed(vec):
+        ret <<= 1
+        ret |= int(bit)
+    return int.to_bytes(ret, 16, 'big')
+
+def gaussian_nullspace(mat):
+    mat = mat.T
+    target = np.eye(mat.shape[0], dtype=np.int8)
+    idx = 0
+    rank = 0
+    for idx in trange(mat.shape[1], desc='Calculating the nullspace', leave=False):
+        if rank == min(mat.shape):
+            break
+        row_idx = np.flatnonzero(mat[rank:, idx]) + rank
+        if len(row_idx) == 0: continue
+        if row_idx[0] != rank:
+            # swap
+            mat[[rank, row_idx[0]]] = mat[[row_idx[0], rank]]
+            target[[rank, row_idx[0]]] = target[[row_idx[0], rank]]
+        # now subtract from the rest
+        for idx_ in row_idx[1:]:
+            mat[idx_, :] = (mat[idx_, :] - mat[rank, :]) % 2
+            target[idx_, :] = (target[idx_, :] - target[rank, :]) % 2
+        rank += 1
+
+    # transpose so column combination is easier
+    target = target[rank:, :].T
+    # remove duplicate vectors
+    target = np.unique(target, axis=1)
+    # remove zero vector if exists
+    target = target[:, np.any(target, axis=0)]
+    return target
+
+def get_Ad(blocks):
+    # higher order/beginning of blocks first, based on Horner's method
+    # remember that this only deals with 2^i-th blocks.
+    acc = np.zeros((128, 128), dtype=np.int8)
+    if len(blocks.shape) == 1: blocks = np.reshape(blocks, (n, 128))
+    for i in range(blocks.shape[0]):
+        acc = ((gf2mat(vec2gf(blocks[i,:])) + acc) @ sqr_mat) % 2
+    return acc
+
+def get_Ad_loc(i):
+    payload = np.zeros((n, 128), dtype=np.int8)
+    payload[divmod(i, 128)] = 1
+    return get_Ad(payload)
+
+def get_dependency_matrix(no_of_zero_rows, X):
+    # rows = bits in Ad*X, col = bits in blocks
+    def get_col(bit_idx):
+        return (get_Ad_loc(bit_idx)[:no_of_zero_rows, :] @ X).flatten() % 2
+    return np.stack(Parallel(n_jobs=12)(delayed(get_col)(row_idx) for row_idx in trange(n * 128, desc='Fetching dependency matrix', leave=False)), axis=1)
+
+# ```
+# [...] [...] [...] [...] [...] size nonce
+# 2^n+1  2^n   2^2    3    2^1
+#   0     1    ...  2^n-2 2^n-1
+# ```
+
+def patch_encrypted(cipher, corrections):
+    # assume evenly padded + number of blocks is 2^n
+    corrections = np.reshape(corrections, (-1, 128))
+    for i in range(n):
+        block_idx = 2 ** n + 1 - 2 ** (n - i)
+        start_idx = block_idx * 16
+        end_idx = start_idx + 16
+        cipher = cipher[:start_idx] + \
+                 vec2block(gf2vec(block2gf(cipher[start_idx : end_idx])) ^ corrections[i, :]) + \
+                 cipher[end_idx:]
+    return cipher
+
+found = Value('b')
+def set_value(val):
+    with found.get_lock(): found.value = val
+
+def try_nullvec(gmac_ok, basis, encrypted, signature):
+    while True:
+        if found.value: break
+        nullvec = (basis @ np.random.randint(2, size=basis.shape[1])) % 2
+        if not nullvec.any(): continue
+        # remove get_Ad_nullvec
+        if gmac_ok(patch_encrypted(encrypted, nullvec), signature):
+        # if not ((get_Ad(nullvec)[:trunc_size] @ authkey) % 2).any():
+            set_value(1)
+            return nullvec
